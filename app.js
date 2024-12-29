96018de4fb5a750c2bd86ad82b58cbf6cbdc6de4
chryswds <dybas.chrys@gmail.com>
2024-12-29 12:18:28 +0000

    getting artist json information from spotify api

--- app.js
+++ app.js
@@ -3,20 +3,30 @@ let { userAccessToken } = require("./spotifyConfig");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  async function getArtists() {
-    let data = await fetch(
-      `https://api.spotify.com/v1/artists/2xvtxDNInKDV4AvGmjw6d1`,
-      {
-        headers: {
-          AUTHORIZATION: `Bearer ${userAccessToken}`,
-        },
-      }
-    );
-    return data;
-  }
+app.use(express.json());
 
-  res.json(getArtists());
+async function getArtists() {
+  let data = await fetch(
+    `https://api.spotify.com/v1/artists/2xvtxDNInKDV4AvGmjw6d1`,
+    {
+      headers: {
+        AUTHORIZATION:
+          `Bearer ` +
+          "BQB3kYbMkXU3VF0AE7rqp7MMk2JxBnGbWOLBh9QI1e1-ZCm6VQ7xV1Fz7YeFhm8ynmchZ52IOnNxlnEh_JF9jdFSqwksybPyxkR579nx8KUXqVFO0cQ",
+      },
+    }
+  );
+  const response = await data.json();
+  return response;
+}
+
+app.get("/", async (req, res) => {
+  try {
+    const profile = await getArtists();
+    res.json(profile);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 app.listen(3000, () => {
