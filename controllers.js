d69ba0f3ac6527bafef6ee7d7a4600ec78816aa2
chryswds <dybas.chrys@gmail.com>
2024-12-30 23:08:46 +0000

    only one fetchArtist implementation now

--- controllers.js
+++ controllers.js
@@ -6,20 +6,6 @@ const mainPage = (req, res) => {
   res.sendFile(path.join(__dirname, "/index.html"));
 };
 
-// const fetchName = async () => {
-//   const response = await fetch(
-//     `https://api.spotify.com/v1/artists/2xvtxDNInKDV4AvGmjw6d1`,
-//     {
-//       headers: {
-//         AUTHORIZATION: `Bearer ` + accessToken,
-//         "Content-Type": "application/json",
-//       },
-//     }
-//   );
-//   const jsonName = await response.json();
-//   return jsonName.name;
-// };
-
 const fetchArtist = async () => {
   const response = await fetch(
     `https://api.spotify.com/v1/artists/2xvtxDNInKDV4AvGmjw6d1`,
@@ -36,29 +22,18 @@ const fetchArtist = async () => {
 
 const returnName = async (req, res) => {
   try {
-    const name = await fetchArtist();
+    const artist = await fetchArtist();
+    const name = artist.name;
     res.json(name);
   } catch (error) {
     console.log("error, ", error);
   }
 };
 
-const fetchImage = async () => {
-  const response = await fetch(
-    `https://api.spotify.com/v1/artists/2xvtxDNInKDV4AvGmjw6d1`,
-    {
-      headers: {
-        AUTHORIZATION: `Bearer ` + accessToken,
-        "Content-Type": "application/json",
-      },
-    }
-  );
-  const jsonImage = await response.json();
-  return jsonImage.images[0];
-};
 const returnImage = async (req, res) => {
   try {
-    const image = await fetchImage();
+    const artist = await fetchArtist();
+    const image = artist.images[0];
     res.json(image);
   } catch (error) {
     res.json({ error: error.message });
