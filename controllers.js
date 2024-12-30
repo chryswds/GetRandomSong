fc318e0f8b1fe974a279fafce0dc931bf6d1646a
chryswds <dybas.chrys@gmail.com>
2024-12-30 22:52:56 +0000

    trying to reduce redundancy - not working

--- controllers.js
+++ controllers.js
@@ -6,7 +6,21 @@ const mainPage = (req, res) => {
   res.sendFile(path.join(__dirname, "/index.html"));
 };
 
-const fetchName = async () => {
+// const fetchName = async () => {
+//   const response = await fetch(
+//     `https://api.spotify.com/v1/artists/2xvtxDNInKDV4AvGmjw6d1`,
+//     {
+//       headers: {
+//         AUTHORIZATION: `Bearer ` + accessToken,
+//         "Content-Type": "application/json",
+//       },
+//     }
+//   );
+//   const jsonName = await response.json();
+//   return jsonName.name;
+// };
+
+const fetchArtist = async () => {
   const response = await fetch(
     `https://api.spotify.com/v1/artists/2xvtxDNInKDV4AvGmjw6d1`,
     {
@@ -16,15 +30,16 @@ const fetchName = async () => {
       },
     }
   );
-  const jsonName = await response.json();
-  return jsonName.name;
+  const artist = await response.json();
+  return artist;
 };
+
 const returnName = async (req, res) => {
   try {
-    const name = await fetchName();
+    const name = await fetchArtist();
     res.json(name);
   } catch (error) {
-    res.json({ error: error.message });
+    console.log("error, ", error);
   }
 };
 
