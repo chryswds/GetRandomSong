fd3cc517c0fe05ef3f9bee9dbe81052d4c07ff21
chryswds <dybas.chrys@gmail.com>
2024-12-30 20:48:03 +0000

    fetching only artist name

--- app.js
+++ app.js
@@ -6,39 +6,63 @@ const accessToken = process.env.ACCESS_TOKEN;
 
 app.use(express.json());
 
-async function getArtists() {
-  let data = await fetch(
+const fetchName = async () => {
+  const response = await fetch(
     `https://api.spotify.com/v1/artists/2xvtxDNInKDV4AvGmjw6d1`,
     {
       headers: {
         AUTHORIZATION: `Bearer ` + accessToken,
+        "Content-Type": "application/json",
       },
     }
   );
-  const actualData = await data.json();
-  return actualData;
-}
+  const jsonName = await response.json();
 
-async function getAlbum() {
-  let data = await fetch(
-    `https://api.spotify.com/v1/albums/4n1tg05JN5EY0k7FRRcAir`,
-    {
-      headers: {
-        AUTHORIZATION: `Bearer ` + accessToken,
-      },
-    }
-  );
-  const response = await data.json();
-  return response;
-}
+  const name = jsonName.name;
+
+  return name;
+};
+
+// async function getArtists() {
+//   let data = await fetch(
+//     `https://api.spotify.com/v1/artists/2xvtxDNInKDV4AvGmjw6d1`,
+//     {
+//       headers: {
+//         AUTHORIZATION: `Bearer ` + accessToken,
+//       },
+//     }
+//   );
+//   const response = await data.json();
+//   return response.artists.name;
+// }
+
+// async function getAlbum() {
+//   let data = await fetch(
+//     `https://api.spotify.com/v1/albums/4n1tg05JN5EY0k7FRRcAir`,
+//     {
+//       headers: {
+//         AUTHORIZATION: `Bearer ` + accessToken,
+//       },
+//     }
+//   );
+//   const response = await data.json();
+//   return response;
+// }
 
 app.get("/", async (req, res) => {
   try {
-    const profile = await getArtists();
+    const profile = await fetchName();
     res.json(profile);
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.json({ error: error.message });
   }
+
+  //   try {
+  //     const profile = await getArtists();
+  //     res.json(profile);
+  //   } catch (error) {
+  //     res.status(500).json({ error: error.message });
+  //   }
 });
 
 app.listen(3000, () => {
