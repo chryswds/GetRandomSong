f3656e8ebe409995a5bd7a04f4afc13679ca4c4f
chryswds <dybas.chrys@gmail.com>
2024-12-30 21:07:26 +0000

    getting image url - gets first image - index[0]

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const express = require("express");
-
 const app = express();
 require("dotenv").config();
 const accessToken = process.env.ACCESS_TOKEN;
-
 app.use(express.json());
 
 const fetchName = async () => {
@@ -17,52 +15,31 @@ const fetchName = async () => {
     }
   );
   const jsonName = await response.json();
-
-  const name = jsonName.name;
-
-  return name;
+  return jsonName.name;
 };
 
-// async function getArtists() {
-//   let data = await fetch(
-//     `https://api.spotify.com/v1/artists/2xvtxDNInKDV4AvGmjw6d1`,
-//     {
-//       headers: {
-//         AUTHORIZATION: `Bearer ` + accessToken,
-//       },
-//     }
-//   );
-//   const response = await data.json();
-//   return response.artists.name;
-// }
-
-// async function getAlbum() {
-//   let data = await fetch(
-//     `https://api.spotify.com/v1/albums/4n1tg05JN5EY0k7FRRcAir`,
-//     {
-//       headers: {
-//         AUTHORIZATION: `Bearer ` + accessToken,
-//       },
-//     }
-//   );
-//   const response = await data.json();
-//   return response;
-// }
+const fetchImage = async () => {
+  const response = await fetch(
+    `https://api.spotify.com/v1/artists/2xvtxDNInKDV4AvGmjw6d1`,
+    {
+      headers: {
+        AUTHORIZATION: `Bearer ` + accessToken,
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  const jsonImage = await response.json();
+  return jsonImage.images[0];
+};
 
 app.get("/", async (req, res) => {
   try {
     const profile = await fetchName();
-    res.json(profile);
+    const image = await fetchImage();
+    res.json(image);
   } catch (error) {
     res.json({ error: error.message });
   }
-
-  //   try {
-  //     const profile = await getArtists();
-  //     res.json(profile);
-  //   } catch (error) {
-  //     res.status(500).json({ error: error.message });
-  //   }
 });
 
 app.listen(3000, () => {
